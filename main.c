f9c8ab0d451dda9f471cc3b2ef9f36a5e997cc9d
Alex <alexsierraalcala@gmail.com>
2023-06-05 22:57:53 -0400

    add define

--- main.c
+++ main.c
@@ -3,3 +3,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <pwd.h>
+#include <signal.h>
+
+#define MAX_SIZE_BUFFER 1024
+#define GREEN "\033[0;32m"
+#define RESET "\033[0m"
+#define ERROR "\033[0;31mmy_ftp\033[0m"
+
